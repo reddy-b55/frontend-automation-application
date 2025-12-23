2d4cb069ae7727630a26bc54679572448633da5c
reddy-b55 <reddy.aahaas@gmail.com>
2025-12-23 17:50:57 +0530

    Update security-groups.tf

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -23,6 +23,13 @@ resource "aws_security_group" "devops_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+ ingress {
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
